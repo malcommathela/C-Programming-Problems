8771a30c815248950b6974e66873906136985a57
malcom_mathela <malcom_mathela.com>
2025-11-26 01:17:04 +0530

    Pointers

---
+++ Pointers.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+    // Basic pointers and Basic addresses
+    int a = 10;
+    int *b = &a;
+
+    printf("%d\n",*b);
+
+
+    //-----------------------------
+    // More advanced use of pointers
+    // Array's are just fancy pointers
+
+    char array[5] = {'a','b','c','d','e'};
+    char *sameArray = array;
+    char **fancy_same_array = &sameArray;
+
+    printf("Array[3]: %c Address: %p\n",array[3],&array[3]);
+    printf("SameArray[3]: %c Address: %p",sameArray[3],&sameArray[3]);
+    printf("**fancy_same_array[3] = %c, addr = %p\n", (*fancy_same_array)[3], &(*fancy_same_array)[3]);
+
+    //------------------------------------
+    //Array of Pointers
+
+    char *ptr_array[2];
+    char *ptr1 = NULL, *ptr2 = NULL;
+    char char_elements[2] = {'a','b'};
+
+    ptr1 = &char_elements[0];
+    ptr2 = &char_elements[1];
+
+    ptr_array[0] = ptr1;
+    ptr_array[1] = ptr2;
+
+    printf("Array[0]: %c Address: %p\n",*ptr_array[0],&ptr1);
+    printf("Array[1]: %c Address: %p\n",*ptr_array[1],&ptr2);
+
+
+
+
+}
