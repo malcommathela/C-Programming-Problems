2e52912cb101057944bde7ef23a2c2bef3aa3b7a
malcom_mathela <malcommathela@gmail.com>
2026-08-05 11:37:30 +0530

    Padding and Logic Fix on 1D Array

--- 1DArray.c
+++ 1DArray.c
@@ -2,19 +2,19 @@
 
 int main(){
 
-    //Initialising array
+    // Initializing array
     int a[20],n;
     printf("Enter number of array elements: \n");
     scanf("%d",&n);
 
 
-    //Assigning values to array
+    // Assigning values to array
     printf("Enter %d array elements: \n", n);
     for(int i = 0; i <= n - 1 ; i++){
         scanf("%d",&a[i]);
     }
 
-    //Printing Array elements
+    // Printing Array elements
     for(int i = 0; i <= n - 1; i++){
         printf("%d ",a[i]);
     }
